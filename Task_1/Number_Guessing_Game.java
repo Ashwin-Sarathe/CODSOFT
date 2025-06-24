a4efdcb8061e4b9e02761de7ed9c70b57be05ff4
Ashwin Sarathe <ashwinsarathe01@gmail.com>
2025-06-24 15:01:40 +0530

    Update Number_Guessing_Game.java

--- Task_1/Number_Guessing_Game.java
+++ Task_1/Number_Guessing_Game.java
@@ -1,53 +1,97 @@
 import java.util.Random;
 import java.util.Scanner;
-
-public class Number_Guessing_Game {
-    public static void main(String[] args) {
-        Random rd = new Random();
-        Scanner sc = new Scanner(System.in);
-        int no_of_rounds =0;
-        int final_score=0;
-        int score=0;
-        int min = 1;
-        int max = 100;
-        int guessNumber=0;
-        int randomNumber = rd.nextInt(max-min+1)+min;
-        char choice = 'Y';
-        do {
-            int numOfAttempts = 10;
-            score=11;
-            no_of_rounds++;
-            do {
-                score--;
-                System.out.println("Please Enter your Guess:");
-                guessNumber = sc.nextInt();
-                int difference = randomNumber - guessNumber;
-                numOfAttempts--;
+class NumberGenerator{
+    Random rd = new Random();
+    int lowerBound=1;
+    int upperBound=100;
+    int randomNumber=0;
+    int generateNum(){
+        randomNumber = rd.nextInt(upperBound-lowerBound+1)+lowerBound;
+        return randomNumber;
+    }
+}
+class Game{
+    int attempts=0;
+    int maxAttempts=10;
+    int targetNumber=0;
+    Game(NumberGenerator obj){
+        targetNumber=obj.generateNum();
+    }
+    boolean isCorrectGuess(int guess){
+        if(guess==targetNumber)
+            return true;
+        else
+            return false;
+    }
+    boolean isOutOfAttempts(){
+        if(attempts==10)
+            return true;
+        else
+            return false;
+    }
+    void incrementAttempts(){
+        attempts++;
+    }
+    int getRemainingAttempts(){
+        return maxAttempts-attempts;
+    }
+    int getTargetNumber(){
+        return targetNumber;
+    }
+}
+class GameRunner{
+    Scanner sc = new Scanner(System.in);
+    int no_of_rounds=0;
+    int guessNumber=0;
+    int difference=0;
+    char choice;
+    int round_score=11,final_score=0;
+    void play(){
+        no_of_rounds++;
+        NumberGenerator ng = new NumberGenerator();
+        Game game = new Game(ng);
+        while(!game.isOutOfAttempts()){
+            System.out.println("Please Enter your Guess:");
+            guessNumber = sc.nextInt();
+            round_score--;
+            game.incrementAttempts();
+            if(game.isCorrectGuess(guessNumber)){
+                System.out.println("Your Guess is Correct!!!!");
+                System.out.println("Round Score: "+round_score);
+                final_score+=round_score;
+                break;
+            }
+            else{
+                System.out.println("Attempts Left: "+game.getRemainingAttempts());
+                difference = game.targetNumber - guessNumber;
                 if (difference >= 30)
                     System.out.println("Your Guess is too Low!!!");
-                else if (difference < 30 && difference > 0)
+                else if (difference > 0)
                     System.out.println("Your Guess is Slightly Low!!!");
                 else if (difference <= -30)
                     System.out.println("Your Guess is too High!!!");
-                else if (difference > -30 && difference < 0)
-                    System.out.println("Your Guess is Slightly High!!!");
                 else
-                    System.out.println("You Guessed Right");
-            } while (guessNumber != randomNumber && numOfAttempts > 0);
-            if(numOfAttempts==0)
-                System.out.println("Sorry!! You Ran Out of Attempts!!");
-            if(score>=8)
-                System.out.println("Bravo!! You Scored "+score);
-            else
-                System.out.println("You Scored "+score);
-            final_score +=score;
-            System.out.println("Ready for another round? (Y/N)");
-            String input = sc.next();
-            choice = input.charAt(0);
-        }while(choice=='Y' || choice=='y');
-        int total_score = no_of_rounds*10;
-        System.out.println("Total number of rounds played: "+no_of_rounds);
-        System.out.println("Final Score => "+final_score+ " out of "+total_score);
-        sc.close();
+                    System.out.println("Your Guess is Slightly High!!!");
+
+            }
+        }
+        if(game.isOutOfAttempts()) {
+            System.out.println("Sorry!! You Ran Out Of Attempts!");
+            System.out.println("The Answer was: "+game.getTargetNumber());
+        }
+        System.out.print("Play again? (y/n): ");
+        choice = sc.next().charAt(0);
+        if(choice=='Y' || choice=='y'){
+            round_score=11;
+            play();
+        }
+        System.out.println("Number of Rounds Played: "+no_of_rounds);
+        System.out.println("You Scored "+final_score+" out of "+no_of_rounds*10);
+    }
+}
+public class Number_Guessing_Game {
+    public static void main(String[] args) {
+        GameRunner gr = new GameRunner();
+        gr.play();
     }
 }
